3dc20c5539b6705f334c034f88a4713629824e55
DENSLnetion <lyoncintakeju@gmail.com>
2026-07-02 21:54:35 +0700

    Add documentation comment to NextLayerApplication
    Added a comment explaining the purpose of the application class.

--- app/src/main/java/com/example/nextlayer/NextLayerApplication.kt
+++ app/src/main/java/com/example/nextlayer/NextLayerApplication.kt
@@ -2,8 +2,13 @@ package com.example.nextlayer
 
 import android.app.Application
 
+/**
+ * File ini wajib ada karena dideklarasikan di AndroidManifest.xml.
+ * Menjaga lifecycle aplikasi tetap stabil dari awal boot.
+ */
 class NextLayerApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        // Tempat inisialisasi library global nantinya jika diperlukan
     }
 }
